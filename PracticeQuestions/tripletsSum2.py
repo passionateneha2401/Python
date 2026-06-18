2025d120071e4494aa980a6e8d83d9f82d5f278f
passionateneha2401 <nehakumbhar2401@gmail.com>
2026-06-18 17:14:28 +0530

    18-06-2026

---
+++ PracticeQuestions/tripletsSum2.py
@@ -0,0 +1,16 @@
+def tripletsSum(arr, n):
+    result = []
+
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                if arr[i] + arr[j] + arr[k] == 0:
+                    result.append([i, j, k])
+
+    return result
+
+arr = [1, -2, 1, 0, 5]
+arr1 = [2, 3, 1, 0, 5]
+
+print(tripletsSum(arr, len(arr)))
+print(tripletsSum(arr1, len(arr1)))
