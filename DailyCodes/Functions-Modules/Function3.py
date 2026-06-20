3600a28b7687dc10c17126058ba4249253c0d7b9
passionateneha2401 <nehakumbhar2401@gmail.com>
2026-06-21 00:00:04 +0530

    Functions&Modules

---
+++ DailyCodes/Functions-Modules/Function3.py
@@ -0,0 +1,15 @@
+
+def main():
+    print("Enter First number: ")
+    value1 = int(input())
+
+    print("Enter second number: ")
+    value2 = int(input())
+
+    Ret = Addition(value1 , value2) #Error--NameError
+
+    print("Addition is: ",Ret)
+
+if __name__ == "__main__":
+    main()
+
---
+++ DailyCodes/Functions-Modules/FunctionModule1.py
@@ -0,0 +1,16 @@
+import MarvellousX
+
+def main():
+    print("Enter First number: ")
+    value1 = int(input())
+
+    print("Enter second number: ")
+    value2 = int(input())
+
+    Ret = MarvellousX.Addition(value1 , value2) 
+
+    print("Addition is: ",Ret)
+
+if __name__ == "__main__":
+    main()
+
