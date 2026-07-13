505fb19dce35380419ad05138cfc6b1104814684
passionateneha2401 <nehakumbhar2401@gmaiil.com>
2026-07-14 00:52:16 +0530

    assg23

---
+++ Assignments/Assg23/Assignment23_2.py
@@ -0,0 +1,31 @@
+'''
+2.Factorial of multiple numbers using Pool.map()
+
+Write a Python program that accepts a list of integers and uses multiprocessing.
+Pool.map() to calculate the factorial of multiple numbers simultaneously.
+
+[10,15,20,25]
+Display:
+1.process id
+2.input number
+3.factorial
+'''
+
+import os
+from multiprocessing import Pool
+
+def Factorial(no):
+    fact = 1
+    for i in range(1,no+1):
+        fact = fact * i
+
+    
+
+def main():
+    Data = [10,15,20,25]
+
+    p = Pool()
+    result=p.map(Factorial,Data)
+
+if __name__ == "__main__":
+    main()
