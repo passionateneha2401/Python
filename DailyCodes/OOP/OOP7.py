d48108b9a9e96d38206395d589ee520c718cd430
passionateneha2401 <nehakumbhar2401@gmaiil.com>
2026-07-13 13:21:15 +0530

    OOP

---
+++ DailyCodes/OOP/OOP7.py
@@ -0,0 +1,31 @@
+class Demo:
+    #class variables
+    value1 = 10
+    value2 = 20
+
+    #instance var
+    def __init__(self):
+        self.no1 = 11
+        self.no2 = 21
+
+    #instance method
+    def fun(self):
+        print("Inside instance method named as fun")
+        print(self.no1)
+        print(self.no2)
+        print(Demo.value1)
+        print(Demo.value2)   #to access class var two ways are there self. & classname.---use classname.---hdfc.roi is better
+    
+    @classmethod
+    def gun(cls):
+        print("Inside instance method named as gun")
+        print(cls.value1)       #Demo = cls
+        print(cls.value2)
+
+    @staticmethod
+    def sun():
+        print("Inside static method named as sun")
+        print(Demo.value1)
+        print(Demo.value2) 
+         
+Demo.sun()
